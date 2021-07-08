69e5dde237354777a9c4f4cb6107d188d78dd2a5
therealsaumil <saumil@saumil.net>
2021-07-09 01:28:43 +0530

    first commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+echo '   _   ___ __  __  ___  _   _ _____   __  | The ARM Exploit Laboratory '
+echo '  /_\ | _ \  \/  |/ _ \| | | | _ \ \ / /  | armx.exploitlab.net'
+echo ' / _ \|   / |\/| | (_) | |_| |   /\ V /   |'
+echo '/_/ \_\_|_\_|  |_|\___/ \___/|_|_\ |_|    | ARM Cross Compiler Container'
+echo
+
+exec "$@"
