0e6343b65f55c3a53a57573477d656ce491c6be2
therealsaumil <saumil@saumil.net>
2022-07-25 14:36:06 +0530

    Changed the banner

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ echo
 echo '   _   ___ __  __  ___  _   _ _____   __  | The Exploit Laboratory '
 echo '  /_\ | _ \  \/  |/ _ \| | | | _ \ \ / /  | emux.exploitlab.net'
 echo ' / _ \|   / |\/| | (_) | |_| |   /\ V /   |'
-echo '/_/ \_\_|_\_|  |_|\___/ \___/|_|_\ |_|    | ARM,MIPS Cross Compiler Container'
+echo '/_/ \_\_|_\_|  |_|\___/ \___/|_|_\ |_|    | ARM,ARM64,MIPS Cross Compiler'
 echo
 
 exec "$@"
